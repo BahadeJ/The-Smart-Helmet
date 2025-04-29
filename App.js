a3420c99d04b947e09112c5d7dbfa96326424d52
BahadeJ <jomarbahade01@gmail.com>
2025-04-29 23:39:33 +0800

    third commit

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 // Import the screens
-import Welcome from './screens/welcome';
-import SignUp from './screens/sign-up';
-import Login from './screens/login';
-import Dashboard from './screens/dashboard';
-import Profile from './screens/profile';
-import Settings from './screens/settings';
-import ViewDetectionHistory from './screens/view-detection-history';
-import HelpScreen from './screens/help';
+import Welcome from "./screens/welcome";
+import SignUp from "./screens/sign-up";
+import Login from "./screens/login";
+import Dashboard from "./screens/dashboard";
+import Profile from "./screens/profile";
+import Settings from "./screens/settings";
+import ViewDetectionHistory from "./screens/view-detection-history";
+import HelpScreen from "./screens/help";
 
 const Stack = createStackNavigator();
 
@@ -20,43 +20,43 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen 
-          name="Welcome" 
-          component={Welcome} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="SignUp" 
-          component={SignUp} 
-          options={{ headerShown: false }} 
-          />
         <Stack.Screen
-          name='Login'
+          name="Welcome"
+          component={Welcome}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
           component={Login}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Dashboard'
+          name="Dashboard"
           component={Dashboard}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Profile'
+          name="Profile"
           component={Profile}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='Settings'
+          name="Settings"
           component={Settings}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='ViewDetectionHistory'
+          name="ViewDetectionHistory"
           component={ViewDetectionHistory}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name='HelpScreen'
+          name="HelpScreen"
           component={HelpScreen}
           options={{ headerShown: false }}
         />
